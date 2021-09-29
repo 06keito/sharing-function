428ab72dfa5b03ec837f007e5fde80b6bf52bb82
06keito <keito.yamamoto06@gmail.com>
2021-09-29 16:34:38 +0900

    Fixed the behavior when reading the last line.

--- Snippet_simplification/user_snippet_simplification.py
+++ Snippet_simplification/user_snippet_simplification.py
@@ -9,10 +9,15 @@
 # -> python user_snippet_simplification.py
 
 def main():
+    line_count = sum(1 for line in open('input.txt'))
     with open('input.txt',mode='r',encoding='utf_8') as f_in,\
     open('output.txt',mode='w',encoding='utf_8') as f_out:
-        for line in f_in.readlines():
-            f_out.write('{}'.format('"'+line.rstrip('\n')+'",\n'))
-    
+        for idx,line in enumerate(f_in.readlines(),1):
+            if idx!=line_count:
+                text = ('{}'.format('"'+line.rstrip('\n')+'",\n'))
+            else: #last line
+                text = ('{}'.format('"'+line.rstrip('\n')+'"'))
+            f_out.write(text)
+
 if __name__ == '__main__':
     main()
